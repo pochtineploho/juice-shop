a2661e85b1a96316b7e83a42fbb4903bc2a73f5a
pochtineploho <Mr.sergei.spb@inbox.ru>
2026-02-26 21:00:10 +0300

    fix: directory listing

--- data/static/codefixes/directoryListingChallenge_3.ts
+++ data/static/codefixes/directoryListingChallenge_3.ts
@@ -1,19 +1,33 @@
-/* /ftp directory browsing */
-  app.use('/ftp', serveIndexMiddleware, serveIndex('ftp', { icons: true }))
+app.use('/ftp',
+  express.static(path.resolve('ftp'), {
+    index: false,
+    redirect: false,
+    dotfiles: 'deny'
+  })
+)
 
-  app.use('/.well-known', serveIndexMiddleware, serveIndex('.well-known', { icons: true, view: 'details' }))
-  app.use('/.well-known', express.static('.well-known'))
+app.use('/.well-known',
+  express.static(path.resolve('.well-known'), {
+    index: false
+  })
+)
 
-  /* /encryptionkeys directory browsing */
-  app.use('/encryptionkeys', serveIndexMiddleware, serveIndex('encryptionkeys', { icons: true, view: 'details' }))
-  app.use('/encryptionkeys/:file', serveKeyFiles())
+app.use('/support/logs',
+  isAuthorized(),
+  isAdmin(),
+  express.static(path.resolve('logs'), {
+    index: false,
+    dotfiles: 'deny'
+  })
+)
 
-  /* /logs directory browsing */
-  app.use('/support/logs', serveIndexMiddleware, serveIndex('logs', { icons: true, view: 'details' }))
-  app.use('/support/logs/:file', serveLogFiles())
+if (process.env.NODE_ENV !== 'production') {
+  app.use('/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+  )
+}
 
-  /* Swagger documentation for B2B v2 endpoints */
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use(express.static(path.resolve('frontend/dist/frontend')))
 
-  app.use(express.static(path.resolve('frontend/dist/frontend')))
-  app.use(cookieParser('kekse'))
+app.use(cookieParser(process.env.COOKIE_SECRET))
