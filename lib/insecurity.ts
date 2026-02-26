c38040c25f509145d474fa9edb9018d859c99fd1
pochtineploho <Mr.sergei.spb@inbox.ru>
2026-02-26 20:48:14 +0300

    fix: hide sensitive credentials

--- lib/insecurity.ts
+++ lib/insecurity.ts
@@ -19,8 +19,11 @@ import * as utils from './utils'
 // @ts-expect-error FIXME no typescript definitions for z85 :(
 import * as z85 from 'z85'
 
-export const publicKey = fs ? fs.readFileSync('encryptionkeys/jwt.pub', 'utf8') : 'placeholder-public-key'
-const privateKey = '-----BEGIN RSA PRIVATE KEY-----\r\nMIICXAIBAAKBgQDNwqLEe9wgTXCbC7+RPdDbBbeqjdbs4kOPOIGzqLpXvJXlxxW8iMz0EaM4BKUqYsIa+ndv3NAn2RxCd5ubVdJJcX43zO6Ko0TFEZx/65gY3BE0O6syCEmUP4qbSd6exou/F+WTISzbQ5FBVPVmhnYhG/kpwt/cIxK5iUn5hm+4tQIDAQABAoGBAI+8xiPoOrA+KMnG/T4jJsG6TsHQcDHvJi7o1IKC/hnIXha0atTX5AUkRRce95qSfvKFweXdJXSQ0JMGJyfuXgU6dI0TcseFRfewXAa/ssxAC+iUVR6KUMh1PE2wXLitfeI6JLvVtrBYswm2I7CtY0q8n5AGimHWVXJPLfGV7m0BAkEA+fqFt2LXbLtyg6wZyxMA/cnmt5Nt3U2dAu77MzFJvibANUNHE4HPLZxjGNXN+a6m0K6TD4kDdh5HfUYLWWRBYQJBANK3carmulBwqzcDBjsJ0YrIONBpCAsXxk8idXb8jL9aNIg15Wumm2enqqObahDHB5jnGOLmbasizvSVqypfM9UCQCQl8xIqy+YgURXzXCN+kwUgHinrutZms87Jyi+D8Br8NY0+Nlf+zHvXAomD2W5CsEK7C+8SLBr3k/TsnRWHJuECQHFE9RA2OP8WoaLPuGCyFXaxzICThSRZYluVnWkZtxsBhW2W8z1b8PvWUE7kMy7TnkzeJS2LSnaNHoyxi7IaPQUCQCwWU4U+v4lD7uYBw00Ga/xt+7+UqFPlPVdz1yyr4q24Zxaw0LgmuEvgU5dycq8N7JxjTubX0MIRR+G9fmDBBl8=\r\n-----END RSA PRIVATE KEY-----'
+export const publicKey = fs.readFileSync(process.env.JWT_PUBLIC_KEY_PATH || 'encryptionkeys/jwt.pub', 'utf8')
+const privateKey = fs.readFileSync(process.env.JWT_PRIVATE_KEY_PATH || 'encryptionkeys/jwt.key', 'utf8')
+
+const HMAC_SECRET = requireEnv('HMAC_SECRET')
+const DELUXE_SECRET = requireEnv('DELUXE_SECRET')
 
 interface ResponseWithUser {
   status?: string
@@ -41,7 +44,7 @@ interface IAuthenticatedUsers {
 }
 
 export const hash = (data: string) => crypto.createHash('md5').update(data).digest('hex')
-export const hmac = (data: string) => crypto.createHmac('sha256', 'pa4qacea4VK9t9nGv7yZtwmj').update(data).digest('hex')
+export const hmac = (data: string) => crypto.createHmac('sha256', HMAC_SECRET).update(data).digest('hex')
 
 export const cutOffPoisonNullByte = (str: string) => {
   const nullByte = '%00'
@@ -149,8 +152,7 @@ export const roles = {
 }
 
 export const deluxeToken = (email: string) => {
-  const hmac = crypto.createHmac('sha256', privateKey)
-  return hmac.update(email + roles.deluxe).digest('hex')
+    return crypto.createHmac('sha256', DELUXE_SECRET).update(email + roles.deluxe).digest('hex')
 }
 
 export const isAccounting = () => {
@@ -198,4 +200,12 @@ export const updateAuthenticatedUsers = () => (req: Request, res: Response, next
     })
   }
   next()
+}
+
+function requireEnv(name: string): string {
+    const value = process.env[name]
+    if (!value) {
+        throw new Error(`${name} must be defined`)
+    }
+    return value
 }
